60030dd14efdcafdd49a88324586ddf15055177c
Murillou <murillovinicius13@hotmail.com>
2025-02-07 14:27:12 -0300

    feat: estruturada a página home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,29 @@
 import { Heading, Text } from '@ignite-ui/react';
+import { Container, Hero, Preview } from './style';
+
+import previewImage from '../../assets/callPreview.png';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <>
-      <Heading> Hello World</Heading>
-    </>
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento descomplicado</Heading>
+        <Text size="lg">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendário simbolizando aplicação em funcionamento"
+        ></Image>
+      </Preview>
+    </Container>
   );
 }
