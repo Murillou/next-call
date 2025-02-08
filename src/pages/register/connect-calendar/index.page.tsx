e7d5fd01abc46ede62d80331c22b0dd339843fbc
Murillou <murillovinicius13@hotmail.com>
2025-02-08 14:45:41 -0300

    feat(register): adicionando estruturação da página de conexão com o Google Agenda

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,22 @@
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react';
+import { Container, Header } from '../styles';
+import { ArrowRight } from 'phosphor-react';
+export default function Register() {
+  async function handleRegister() {}
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conecte sua agenda!</Heading>
+        <Text>
+          Conecte o seu calendário para verificar automaticamente as horas
+          ocupadas e os novos eventos à medida em que são agendados.
+        </Text>
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+      <Button type="submit">
+        Próximo passo <ArrowRight />
+      </Button>
+    </Container>
+  );
+}
