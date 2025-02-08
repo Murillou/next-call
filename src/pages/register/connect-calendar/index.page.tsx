8ee0ee5a8cab1d2945dd84268c22c4b7f0ff10df
Murillou <murillovinicius13@hotmail.com>
2025-02-08 14:57:11 -0300

    style(register): adicionando estilização á página de conectar com o Google Agenda

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,6 +1,7 @@
 import { Button, Heading, MultiStep, Text } from '@ignite-ui/react';
 import { Container, Header } from '../styles';
 import { ArrowRight } from 'phosphor-react';
+import { ConnectBox, ConnectItem } from './styles';
 export default function Register() {
   async function handleRegister() {}
 
@@ -14,9 +15,18 @@ export default function Register() {
         </Text>
         <MultiStep size={4} currentStep={2} />
       </Header>
-      <Button type="submit">
-        Próximo passo <ArrowRight />
-      </Button>
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          <Button variant="secondary" size="sm">
+            Conectar <ArrowRight />
+          </Button>
+        </ConnectItem>
+
+        <Button type="submit">
+          Próximo passo <ArrowRight />
+        </Button>
+      </ConnectBox>
     </Container>
   );
 }
