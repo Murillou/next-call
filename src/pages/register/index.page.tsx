b24bb615cff653a40ae32793a20ae282e48790b2
Murillou <murillovinicius13@hotmail.com>
2025-02-07 22:33:31 -0300

    feat(users): adicionando requisção à API no registro e validação de método

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -7,6 +7,7 @@ import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import { api } from '@/lib/axios';
 
 const registerFormSchema = z.object({
   username: z
@@ -42,7 +43,14 @@ export default function Register() {
   }, [router.query.username, setValue]);
 
   async function handleRegister(data: RegisterFormData) {
-    console.log(data);
+    try {
+      await api.post('/users', {
+        fullName: data.fullName,
+        username: data.username,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
