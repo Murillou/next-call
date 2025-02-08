a8c8001f87df8fcbb7faf8a392656a6ac912dd04
Murillou <murillovinicius13@hotmail.com>
2025-02-07 23:01:46 -0300

    feat(users): adicionando manipulação de cookie para identificar o usuário

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { prisma } from '@/lib/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { setCookie } from 'nookies';
 
 export default async function handler(
   req: NextApiRequest,
@@ -23,6 +24,11 @@ export default async function handler(
       fullName,
       username,
     },
+  });
+
+  setCookie({ res }, '@nextcall:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+    path: '/',
   });
 
   return res.status(201).json(user);
