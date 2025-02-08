ca56ee34a23857b5924bbc490db4079cb1c1c269
Murillou <murillovinicius13@hotmail.com>
2025-02-07 22:44:27 -0300

    feat(users): adicionando criação de usuário ao banco de dados

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,9 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { prisma } from '@/lib/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method !== 'POST') {
     return res.status(405).end();
   }
-  return res.status(200).json(req.body);
+
+  const { fullName, username } = req.body;
+
+  const user = await prisma.user.create({
+    data: {
+      fullName,
+      username,
+    },
+  });
+
+  return res.status(201).json(user);
 }
