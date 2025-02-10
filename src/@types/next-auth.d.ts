f55a5981e5d93aed491a7b7e1e59e558fb0b12e7
Murillou <murillovinicius13@hotmail.com>
2025-02-10 18:35:25 -0300

    feat(auth): implementando PrismaAdapter no NextAuth para autenticação com o google

---
+++ src/@types/next-auth.d.ts
@@ -0,0 +1,10 @@
+import NextAuth from 'next-auth';
+declare module 'next-auth' {
+  export interface User {
+    id: string;
+    fullName: string;
+    email: string;
+    username: string;
+    avatar_url: string;
+  }
+}
